95e103b971fc69ae8b28998d2c69a17d4a1c441a
nickarvinlobaton <nick.l@agentsofvalue.com>
2020-08-05 13:30:12 +0800

    added default layout

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,62 +1,102 @@
 <template>
   <div>
-    <Nuxt />
+    <a-layout id="components-layout-demo-custom-trigger">
+      <a-layout-sider
+        v-if="sidebar"
+        v-model="collapsed"
+        :trigger="null"
+        collapsible>
+        <div class="logo"/>
+        <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
+          <a-menu-item key="1">
+            <a-icon type="user"/>
+            <span>nav 1</span>
+          </a-menu-item>
+          <a-menu-item key="2">
+            <a-icon type="video-camera"/>
+            <span>nav 2</span>
+          </a-menu-item>
+          <a-menu-item key="3">
+            <a-icon type="upload"/>
+            <span>nav 3</span>
+          </a-menu-item>
+        </a-menu>
+      </a-layout-sider>
+      <a-layout>
+        <a-layout-header :style="{background: headerColor, padding: 0}">
+          <a-icon
+            v-if="sidebar"
+            class="trigger"
+            :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+            @click="() => (collapsed = !collapsed)"
+          />
+          <div v-if="!sidebar">
+            <a-button
+              type="link"
+              class="auth-link"
+              style="margin-right: 10px"
+              @click="$router.push('/login', () => {})"
+            >
+              Login
+            </a-button>
+            <a-button
+              type="link"
+              class="auth-link"
+              @click="$router.push('/register', () => {})"
+            >
+              Register
+            </a-button>
+          </div>
+        </a-layout-header>
+        <a-layout-content
+          :style="{ margin: '24px 16px', padding: '24px', background: '#fff', minHeight: '75vh' }"
+        >
+          <Nuxt/>
+        </a-layout-content>
+        <a-layout-footer></a-layout-footer>
+      </a-layout>
+    </a-layout>
   </div>
 </template>
 
+<script>
+  export default {
+    data() {
+      return {
+        collapsed: false,
+        sidebar: true,
+      };
+    },
+    computed: {
+      headerColor() {
+        return this.sidebar ? '#fff' : '#001529'
+      },
+    }
+  }
+</script>
+
 <style>
-html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+  #components-layout-demo-custom-trigger .trigger {
+    font-size: 18px;
+    line-height: 64px;
+    padding: 0 24px;
+    cursor: pointer;
+    transition: color 0.3s;
+  }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
+  #components-layout-demo-custom-trigger .trigger:hover {
+    color: #1890ff;
+  }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
-}
+  #components-layout-demo-custom-trigger .logo {
+    height: 32px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 16px;
+  }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
-}
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+  .auth-link {
+    float: right;
+    padding: 16px 14px;
+    color: #ffffff
+  }
 </style>
---
+++ client/pages/register.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Register</div>
+</template>
+
+<script>
+  export default {
+    name: "register"
+  }
+</script>
+
+<style scoped>
+
+</style>
